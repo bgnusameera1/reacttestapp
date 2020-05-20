8df67efc975afa8fca5caa8bda83e4e37d74fddb
Nuwan Sameera <bgnusameera1@gmail.com>
2020-05-20 10:38:07 +0530

    new

---
+++ Builder/src/config/firebase_config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCfLbz2Aec65pgdK0EoJzMHA-0JQv0H_PI",
+  authDomain: "reactappbuilder-4ad17.firebaseapp.com",
+  databaseURL: "https://reactappbuilder-4ad17.firebaseio.com",
+  projectId: "reactappbuilder-4ad17",
+  storageBucket: "reactappbuilder-4ad17.appspot.com",
+  messagingSenderId: "539869933569",
+  appId: "1:539869933569:web:2a3ca3e371174a03f6d000",
+  measurementId: "G-ZKJB28RG1B"
+}; 
+exports.config = firebaseConfig;
